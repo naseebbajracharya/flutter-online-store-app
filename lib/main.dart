1b04beb7fa0781b91e9ebd11466d3ec4f61f2061
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2020-05-19 21:31:28 +0545

    sign-in floating button conditions fixed

--- lib/main.dart
+++ lib/main.dart
@@ -37,10 +37,14 @@ class MyApp extends StatelessWidget {
             StoreProvider.of<AppState>(context).dispatch(getUserAction);
             //dispatch an action (getUserAction) to grab user data
             StoreProvider.of<AppState>(context).dispatch(getProductsAction);
+            StoreProvider.of<AppState>(context).dispatch(getCartProductsAction);
+
 
           }
         ),
-        '/cart':(BuildContext context) => CartPage()
+        '/cart':(BuildContext context) => CartPage(onInit: (){
+          StoreProvider.of<AppState>(context).dispatch(getCardsAction);
+        })
       },
       theme: ThemeData(
         // This is the theme of your application.
--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_online_store/models/app_state.dart';
 import 'package:flutter_online_store/models/product.dart';
 import 'package:flutter_online_store/pages/products_page.dart';
+import 'package:flutter_online_store/redux/actions.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 
 
 class ProductDetailPage extends StatelessWidget {
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
   final Product item;
   ProductDetailPage({ this.item });
+
+  bool _isInCart(AppState state, String id){
+    final List<Product> cartProducts = state.cartProducts;
+    return cartProducts.indexWhere((cartProduct) => cartProduct.id == id) > - 1;
+  }
 
   @override
 
@@ -13,6 +22,7 @@ class ProductDetailPage extends StatelessWidget {
     final String pictureUrl = 'http://10.0.2.2:1337${item.picture['url']}';
     final Orientation orientation = MediaQuery.of(context).orientation;
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text(item.name)
       ),
@@ -28,6 +38,22 @@ class ProductDetailPage extends StatelessWidget {
           ),
           Text(item.name, style: Theme.of(context).textTheme.title),
           Text('\Rs.${item.price}', style: Theme.of(context).textTheme.body1),
+          Padding(padding: EdgeInsets.symmetric(horizontal: 32.0),
+          child: StoreConnector<AppState, AppState>(
+          converter:(store) => store.state,
+          builder: (_, state){
+            return state.user != null ? IconButton(icon: Icon(Icons.add_shopping_cart), 
+            color: _isInCart(state, item.id) ? Colors.red[400] : Colors.black,
+            onPressed: () {
+              StoreProvider.of<AppState>(context).dispatch(toggleCartProductAction(item));
+              final snackBar = SnackBar(
+                duration: Duration(seconds: 2),
+                content: Text('Your Cart Has Been Updated!'),
+              );
+              _scaffoldKey.currentState.showSnackBar(snackBar);
+            }
+            ) : Text('');
+          }),),
           Flexible(child: SingleChildScrollView(child: Padding(
             child: Text(item.description),
             padding: EdgeInsets.only(left: 23.0, right: 23.0, bottom: 25.0 ))))
