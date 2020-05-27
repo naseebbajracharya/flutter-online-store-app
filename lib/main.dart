91cb73c858fe0f46a5d2002137e227ac0b8b2b66
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2020-05-27 21:06:57 +0545

    cart controller updates2

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,13 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_online_store/models/app_state.dart';
-import 'package:flutter_online_store/pages/cart_page.dart';
-import 'package:flutter_online_store/redux/actions.dart';
-import 'package:flutter_online_store/redux/reducers.dart';
-import 'package:flutter_online_store/pages/login_page.dart';
-import 'package:flutter_online_store/pages/products_page.dart';
-import 'package:flutter_online_store/pages/register_page.dart';
-
-import 'package:redux/redux.dart';
-import 'package:flutter_redux/flutter_redux.dart';
-import 'package:redux_logging/redux_logging.dart';
-import 'package:redux_thunk/redux_thunk.dart';
-
-void main() {
-  final store = Store<AppState>(appReducer, initialState: AppState.initial(), middleware: [thunkMiddleware, LoggingMiddleware.printer()]);
-  runApp(MyApp(store: store));
-} 
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final Store<AppState> store;
-  MyApp({ this.store });
   // This widget is the root of your application.
-  
   @override
   Widget build(BuildContext context) {
-    return StoreProvider (
-      store: store,
-      child: MaterialApp(
-      title: 'Online Store',
-      routes: {
-        '/login':(BuildContext context) => LoginPage(),
-        '/register':(BuildContext context) =>RegisterPage(),
-        '/':(BuildContext context) => ProductsPage(
-          onInit: (){
-            StoreProvider.of<AppState>(context).dispatch(getUserAction);
-            //dispatch an action (getUserAction) to grab user data
-            StoreProvider.of<AppState>(context).dispatch(getProductsAction);
-            StoreProvider.of<AppState>(context).dispatch(getCartProductsAction);
-
-
-          }
-        ),
-        '/cart':(BuildContext context) => CartPage(onInit: (){
-          StoreProvider.of<AppState>(context).dispatch(getCardsAction);
-        })
-      },
+    return MaterialApp(
+      title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -56,17 +18,94 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primaryColor: Colors.blueGrey[600],
-        accentColor: Colors.teal[500],
-        textTheme: TextTheme(
-          headline: TextStyle(fontSize: 46.0, fontWeight: FontWeight.bold),
-          title: TextStyle(fontSize: 31.0),
-          body1: TextStyle(fontSize: 22.0)
-        )
+        primarySwatch: Colors.blue,
       ),
-      //home: MyHomePage(title: 'Flutter'),
-      
-    ));
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
